9e88c85e879284dc24d9538a34e3164156fa4fcb
Bot <chrisbbo812@gmail.com>
2024-09-03 10:14:37 +0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,21 +1,18 @@
 @echo off
 
-REM 克隆仓库
-git clone https://github.com/boombbo/batch_run
+REM 设置虚拟环境目录路径为当前项目根目录下的 "venv" 文件夹
+set VENV_DIR=%~dp0venv
 
-REM 进入目录
-cd batch_run
-
-REM 创建虚拟环境
-python -m venv venv
+REM 创建虚拟环境到指定目录
+python -m venv "%VENV_DIR%"
 
 REM 激活虚拟环境
-call venv\Scripts\activate
+call "%VENV_DIR%\Scripts\activate"
+
+REM 升级pip
+python.exe -m pip install --upgrade pip
 
 REM 安装依赖
 pip install -r requirements.txt
-
-REM 启动主程序
-python main.py
 
 pause
